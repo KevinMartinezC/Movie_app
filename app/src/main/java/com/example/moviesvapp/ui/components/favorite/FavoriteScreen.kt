a0cec018c51e844f84aab94ee9fb6ce0aed2bae6
kevinMartinezC <ceronk003@gmail.com>
2023-04-28 14:43:34 -0600

    feat: show a bottomsheet with the details of movies when the image is clicked in the favorite screen

--- app/src/main/java/com/example/moviesvapp/ui/components/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/example/moviesvapp/ui/components/favorite/FavoriteScreen.kt
@@ -3,9 +3,14 @@ package com.example.moviesvapp.ui.components.favorite
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerDefaults
@@ -13,15 +18,24 @@ import androidx.compose.foundation.pager.PagerSnapDistance
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
@@ -72,7 +86,7 @@ fun FavoriteScreen(favoriteMoviesFlow: StateFlow<List<FavoriteMovie>>) {
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun FavoriteMovieItem(
     favoriteMovie: FavoriteMovie,
@@ -82,6 +96,7 @@ fun FavoriteMovieItem(
     cardHeight: Dp
 ) {
     val painter = rememberAsyncImagePainter(model = favoriteMovie.poster)
+    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
 
     val pageOffset = (
             (pagerState.currentPage - currentPage) + pagerState.currentPageOffsetFraction
@@ -98,12 +113,66 @@ fun FavoriteMovieItem(
             .size(cardWidth, cardHeight)
             .scale(scale)
             .animateContentSize()
+            .clickable(onClick = {
+            openBottomSheet = !openBottomSheet
+        }),
     ) {
         Image(
             painter = painter,
             contentDescription = stringResource(R.string.movie_poster),
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.Crop
+        )
+    }
+    if (openBottomSheet) {
+        val sheetState = rememberModalBottomSheetState()
+        ModalBottomSheet(
+            onDismissRequest = { openBottomSheet = false },
+            modifier = Modifier.fillMaxSize(),
+            sheetState = sheetState,
+            content = {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = rememberAsyncImagePainter(model = favoriteMovie.poster),
+                        contentDescription = stringResource(R.string.movie_poster),
+                        modifier = Modifier.height(200.dp),
+                        contentScale = ContentScale.Crop
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(
+                        text = stringResource(R.string.category),
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold
+
+                    )
+                    Text(
+                        favoriteMovie.type,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(
+                        text = stringResource(R.string.release_date),
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold
+
+                    )
+                    Text(
+                        favoriteMovie.year,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+                }
+            }
         )
     }
 }
