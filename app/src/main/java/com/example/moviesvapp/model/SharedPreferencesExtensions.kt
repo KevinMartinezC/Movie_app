a5dabf871da4d490c54c0f02b2657563e248aaf0
kevinMartinezC <ceronk003@gmail.com>
2023-04-28 15:05:17 -0600

    feat: organize code

--- app/src/main/java/com/example/moviesvapp/model/SharedPreferencesExtensions.kt
+++ app/src/main/java/com/example/moviesvapp/model/SharedPreferencesExtensions.kt
@@ -3,6 +3,9 @@ package com.example.moviesvapp.model
 import android.content.SharedPreferences
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.example.moviesvapp.model.SharedPreferencesExtensionsKey.API_KEY
+import com.example.moviesvapp.model.SharedPreferencesExtensionsKey.LAST_LOGIN_DATE
+import com.example.moviesvapp.model.SharedPreferencesExtensionsKey.USER_NAME
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -20,27 +23,27 @@ fun SharedPreferences.saveApiKey(apiKey: String) {
 }
 
 fun SharedPreferences.getApiKey(): String? {
-    return getString("API_KEY", null)
+    return getString(API_KEY, null)
 }
 
 fun SharedPreferences.clearUsername() {
-    edit().remove("USERNAME").apply()
+    edit().remove(USER_NAME).apply()
 }
 
 fun SharedPreferences.clearApiKey() {
-    edit().remove("API_KEY").apply()
+    edit().remove(API_KEY).apply()
 }
 
 fun SharedPreferences.saveLastLoginDate(date: String) {
-    edit().putString("LAST_LOGIN_DATE", date).apply()
+    edit().putString(LAST_LOGIN_DATE, date).apply()
 }
 
 fun SharedPreferences.getLastLoginDate(): String? {
-    return getString("LAST_LOGIN_DATE", null)
+    return getString(LAST_LOGIN_DATE, null)
 }
 
 fun SharedPreferences.clearLastLoginDate() {
-    edit().remove("LAST_LOGIN_DATE").apply()
+    edit().remove(LAST_LOGIN_DATE).apply()
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -48,4 +51,10 @@ fun getCurrentDateTime(): String {
     val currentDateTime = LocalDateTime.now()
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
     return currentDateTime.format(formatter)
+}
+
+object SharedPreferencesExtensionsKey{
+    const val API_KEY ="API_KEY"
+    const val  USER_NAME = "USERNAME"
+    const val  LAST_LOGIN_DATE = "LAST_LOGIN_DATE"
 }
--- app/src/main/java/com/example/moviesvapp/ui/components/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/example/moviesvapp/ui/components/favorite/FavoriteScreen.kt
@@ -1,43 +1,23 @@
 package com.example.moviesvapp.ui.components.favorite
 
-import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerDefaults
 import androidx.compose.foundation.pager.PagerSnapDistance
-import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
-import com.example.moviesvapp.R
 import com.example.moviesvapp.model.database.FavoriteMovie
-import com.example.moviesvapp.ui.components.MovieDetails
 import com.example.moviesvapp.ui.theme.MyApplicationTheme
 import kotlinx.coroutines.flow.StateFlow
-import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -80,61 +60,3 @@ fun FavoriteScreen(favoriteMoviesFlow: StateFlow<List<FavoriteMovie>>) {
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
-@Composable
-fun FavoriteMovieItem(
-    favoriteMovie: FavoriteMovie,
-    pagerState: PagerState,
-    currentPage: Int,
-    cardWidth: Dp,
-    cardHeight: Dp
-) {
-    val painter = rememberAsyncImagePainter(model = favoriteMovie.poster)
-    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
-
-    val pageOffset = (
-            (pagerState.currentPage - currentPage) + pagerState.currentPageOffsetFraction
-            ).absoluteValue
-
-    val scale = lerp(
-        start = 0.8f,
-        stop = 1f,
-        fraction = 1f - pageOffset.coerceIn(0f, 1f)
-    )
-
-    Card(
-        modifier = Modifier
-            .size(cardWidth, cardHeight)
-            .scale(scale)
-            .animateContentSize()
-            .clickable(onClick = {
-            openBottomSheet = !openBottomSheet
-        }),
-    ) {
-        Image(
-            painter = painter,
-            contentDescription = stringResource(R.string.movie_poster),
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop
-        )
-    }
-    if (openBottomSheet) {
-        val sheetState = rememberModalBottomSheetState()
-        ModalBottomSheet(
-            onDismissRequest = { openBottomSheet = false },
-            modifier = Modifier.fillMaxSize(),
-            sheetState = sheetState,
-            content = {
-                MovieDetails(
-                    poster = favoriteMovie.poster,
-                    type = favoriteMovie.type,
-                    year = favoriteMovie.year
-                )
-            }
-        )
-    }
-}
-
-fun lerp(start: Float, stop: Float, fraction: Float): Float {
-    return (1 - fraction) * start + fraction * stop
-}
